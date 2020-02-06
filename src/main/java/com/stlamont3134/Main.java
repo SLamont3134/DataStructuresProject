ee7f7dc8f77ddb3e4cf17e72832a259fcdc52997
SLamont <stlamont3134@eagle.fgcu..edu>
2020-02-06 17:21:56 -0500

    Iteration 1: Create a Maven project with OpenCSV working & read & print the contents of a file (an action called parsing).

--- src/main/java/com/stlamont3134/Main.java
+++ src/main/java/com/stlamont3134/Main.java
@@ -5,8 +5,8 @@ public class Main {
 
   public static void main(String[] args){
 
-
     //CSV Parsing Iteration 1
+    //Iteration 1: Create a Maven project with OpenCSV working & read & print the contents of a file (an action called parsing).
    CSVParsing parser = new CSVParsing("src/main/java/com/stlamont3134/SEOExample.csv");
    parser.printFile();
 
